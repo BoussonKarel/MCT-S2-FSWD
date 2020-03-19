5246631813201ba5e4872f536be802e4978e7f2c
BoussonKarel <BoussonKarel@users.noreply.github.com>
2020-03-19 14:35:32 +0100

    Backend week 6

--- 06 Recap/backend/app.py
+++ 06 Recap/backend/app.py
@@ -15,10 +15,78 @@ endpoint = '/api/v1'
 def index():
     return "PLEASE VISIT API ROUTE"
 
-
 @app.route(endpoint+'/')
 def api():
     return "Welcome to API"
+
+
+@app.route(endpoint+'/bestemmingen', methods=['GET'])
+def bestemmingen():
+    # GET: alle treinen ophalen
+    return jsonify(DataRepository.read_bestemmingen()), 200
+
+
+@app.route(endpoint+'/treinen', methods=['GET', 'POST'])
+def treinen():
+    # GET: alle treinen ophalen
+    if request.method == 'GET':
+        return jsonify(DataRepository.read_treinen()), 200
+    # POST: trein toevoegen
+    elif request.method == 'POST':
+        gegevens = DataRepository.json_or_formdata(request)
+        data = DataRepository.create_trein(
+            gegevens["vertrek"],
+            gegevens["bestemmingID"],
+            gegevens["spoor"],
+            gegevens["vertraging"],
+            gegevens["afgeschaft"]
+            )
+        return jsonify(TreinID=data), 201
+
+
+@app.route(endpoint+'/treinen/<trein_id>', methods=['GET', 'PUT', 'DELETE'])
+def trein(trein_id):
+    # GET: specifieke trein ophalen
+    if request.method == 'GET':
+        return jsonify(DataRepository.read_trein(trein_id)), 200
+    # PUT: trein informatie updaten
+    elif request.method == 'PUT':
+        gegevens = DataRepository.json_or_formdata(request)
+        data = DataRepository.update_trein(
+            gegevens["vertrek"],
+            gegevens["bestemmingID"],
+            gegevens["spoor"],
+            gegevens["vertraging"],
+            gegevens["afgeschaft"],
+            trein_id
+            )
+        if data is not None:
+            if data > 0:
+                return jsonify(idtrein = trein_id), 200
+            else:
+                return jsonify(status = data), 200
+        else:
+            return jsonify(message = "error"), 404
+    # DELETE: trein verwijderen
+    elif request.method == 'DELETE':
+        data = DataRepository.delete_trein(trein_id)
+        return jsonify(data)
+
+
+@app.route(endpoint+'/treinen/<trein_id>/vertraging', methods=['PUT'])
+def trein_vertraging(trein_id):
+    gegevens = DataRepository.json_or_formdata(request)
+    data = DataRepository.update_trein_vertraging(trein_id, gegevens["vertraging"])
+    if data is not None:
+        return jsonify(status="OK"), 200
+    else:
+        return jsonify(message="Error"), 404
+
+
+@app.route(endpoint+'/treinen/bestemming/<bestemming_id>', methods=['GET'])
+def treinen_met_bestemming(bestemming_id):
+    # GET: alle treinen ophalen met bestemming _
+    return jsonify(DataRepository.read_treinen_met_bestemming(bestemming_id)), 200
 
 
 # Start app
--- 06 Recap/backend/repositories/DataRepository.py
+++ 06 Recap/backend/repositories/DataRepository.py
@@ -11,5 +11,49 @@ class DataRepository:
         return gegevens
 
     #########  Treinen  #########
+    @staticmethod
+    def read_treinen():
+        sql = "SELECT * FROM treinen"
+        return Database.get_rows(sql)
+
+    @staticmethod
+    def read_trein(idtrein):
+        sql = "SELECT * FROM treinen WHERE idtrein = %s"
+        params = [ idtrein ]
+        return Database.get_one_row(sql, params)
+
+    @staticmethod
+    def read_treinen_met_bestemming(bestemmingid):
+        sql = "SELECT * FROM treinen WHERE bestemmingid = %s"
+        params = [ bestemmingid ]
+        return Database.get_rows(sql, params)
+
+    @staticmethod
+    def create_trein(vertrek, bestemmingid, spoor, vertraging, afgeschaft):
+        sql = "INSERT INTO treinen (vertrek, bestemmingid, spoor, vertraging, afgeschaft) VALUES (%s, %s, %s, %s, %s)"
+        params = [ vertrek, bestemmingid, spoor, vertraging, afgeschaft ]
+        return Database.execute_sql(sql, params)
+
+    @staticmethod
+    def update_trein(vertrek, bestemmingid, spoor, vertraging, afgeschaft, idtrein):
+        sql = "UPDATE treinen SET vertrek = %s, bestemmingid = %s, spoor = %s, vertraging = %s, afgeschaft = %s WHERE idtrein = %s"
+        params = [ vertrek, bestemmingid, spoor, vertraging, afgeschaft, idtrein ]
+        return Database.execute_sql(sql, params)
+
+    @staticmethod
+    def delete_trein(idtrein):
+        sql = "DELETE FROM treinen WHERE idtrein = %s"
+        params = [ idtrein ]
+        return Database.execute_sql(sql, params)
+
+    @staticmethod
+    def update_trein_vertraging (idtrein, vertraging):
+        sql = "UPDATE treinen SET vertraging = %s WHERE idtrein = %s"
+        params = [ vertraging, idtrein ]
+        return Database.execute_sql(sql, params)
 
     #########  Bestemmingen  #########
+    @staticmethod
+    def read_bestemmingen():
+        sql = "SELECT * FROM bestemmingen"
+        return Database.get_rows(sql)
