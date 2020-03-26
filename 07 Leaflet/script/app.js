4bc21b510af015065283deb27e9f42e330fc4ac6
BoussonKarel <BoussonKarel@users.noreply.github.com>
2020-03-26 14:05:06 +0100

    Week 7 Leaflet oef 1

--- 07 Leaflet/script/app.js
+++ 07 Leaflet/script/app.js
@@ -3,9 +3,13 @@
 const provider = "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png";
 const copyright =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>';
+let map, layergroup;
 
 const init = function() {
   console.log("init initiated!");
+
+  map = L.map('mapid').setView([51.041028, 3.398512], 10);
+  L.tileLayer(provider, { attribution: copyright }).addTo(map);
 };
 
 document.addEventListener("DOMContentLoaded", init);
