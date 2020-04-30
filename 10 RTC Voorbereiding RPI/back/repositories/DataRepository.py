bca0501aa3355c869a60195237687166d1e781c7
BoussonKarel <BoussonKarel@users.noreply.github.com>
2020-04-30 13:45:57 +0200

    Added sql statements

--- 10 RTC Voorbereiding RPI/back/repositories/DataRepository.py
+++ 10 RTC Voorbereiding RPI/back/repositories/DataRepository.py
@@ -10,4 +10,27 @@ class DataRepository:
             gegevens = request.form.to_dict()
         return gegevens
 
+    @staticmethod
+    def read_status_lampen():
+        sql = "SELECT * FROM lampen"
+        return Database.get_rows(sql)
+
+    @staticmethod
+    def read_status_lamp_by_id(id):
+        sql = "SELECT * FROM lampen WHERE id = %s"
+        params = [ id ]
+        return Database.get_one_row(sql, params)
+
+    @staticmethod
+    def update_status_lamp(id, status):
+        sql = "UPDATE lampen SET status = %s WHERE id = %s"
+        params = [ status, id ]
+        return Database.execute_sql(sql, params)
+
+    @staticmethod
+    def update_status_alle_lampen(status):
+        sql = "UPDATE lamen SET status = %s"
+        params = [ status ]
+        return Database.execute_sql(sql, params)
+
     # vul hier de verschillende functies aan om je database aan te spreken
